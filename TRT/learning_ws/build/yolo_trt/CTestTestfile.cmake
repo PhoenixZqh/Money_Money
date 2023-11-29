a3df6766dfe11e3c353e8566179bd4d34b3706f2
PhoenixZqh <1173688576@qq.com>
2023-11-29 14:54:58 +0800

    将cuda trt学习的移动到CUDA-TRT仓库

--- TRT/learning_ws/build/yolo_trt/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zqh/PhoenixZ/Money_Cuda_TensorRT/learning_ws/src/yolo_trt
-# Build directory: /home/zqh/PhoenixZ/Money_Cuda_TensorRT/learning_ws/build/yolo_trt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
