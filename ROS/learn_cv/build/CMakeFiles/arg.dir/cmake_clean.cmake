61210481932d3957b2e5203680741277e2a7c0bf
PhoenixZqh <1173688576@qq.com>
2024-05-06 15:42:35 +0800

    add opencv parser

---
+++ ROS/learn_cv/build/CMakeFiles/arg.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arg.dir/src/cv_arg.cpp.o"
+  "CMakeFiles/arg.dir/src/cv_arg.cpp.o.d"
+  "devel/lib/learn_cv/arg"
+  "devel/lib/learn_cv/arg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
