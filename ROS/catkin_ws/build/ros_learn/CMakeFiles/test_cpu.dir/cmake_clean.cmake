de06a2b530c941478bb88efb5c7028ca0c25c5ef
PhoenixZqh <1173688576@qq.com>
2024-07-26 16:12:02 +0800

    添加单目测距积累

---
+++ ROS/catkin_ws/build/ros_learn/CMakeFiles/test_cpu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/Money_Money/ROS/catkin_ws/devel/lib/ros_learn/test_cpu"
+  "/home/Money_Money/ROS/catkin_ws/devel/lib/ros_learn/test_cpu.pdb"
+  "CMakeFiles/test_cpu.dir/src/test_cpu.cpp.o"
+  "CMakeFiles/test_cpu.dir/src/test_cpu.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_cpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
